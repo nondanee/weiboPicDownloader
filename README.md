2d5b5d9162895a372d3574615bff82a64870309b
nondanee <iminezn5656@outlook.com>
2017-12-24 19:50:24 +0800

    api adjustment

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # 免登录下载微博图片(Python2移植版)
 
-批量下载特定用户的高清大图  
+批量下载微博图片
 移植自Java项目 [yAnXImIN/weiboPicDownloader](https://github.com/yAnXImIN/weiboPicDownloader)
+~~骗star~~
 
 ## 更新日志
+
+2017/12/24
+- 接口变更
+- 优化代码
 
 2017/11/14  
 - 优化代码
@@ -22,10 +27,10 @@
 - 更正一处编码错误
 - 优化代码  
 
-## 运行
+## 下载
 
-### 下载可执行文件
-转到[Release](https://github.com/nondanee/weiboPicDownloader/releases)标签下载，使用PyInstaller打包
+### 开箱即用
+转到 [Release](https://github.com/nondanee/weiboPicDownloader/releases) 下载，使用PyInstaller打包
 
 ### 克隆源码
 ```
@@ -33,19 +38,8 @@ git clone https://github.com/nondanee/weiboPicDownloader.git && cd weiboPicDownl
 pip install futures
 python weiboPicDownloader.py
 ```
-*无法运行? 那很有可能是我写崩了, 来提issue或者自己动手丰衣足食*
 
-## 使用
-
-*多线程下载较占带宽, 可能影响正常上网*
-
-1. 输入要保存图片的地址
-2. 输入 3 当然你也可以输入1和2, 具体可以看[原作者(yAnXImIN)](https://github.com/yAnXImIN/)的[这篇博客](http://blog.yanximin.site/2017/09/05/weibo-userid-containerid/)
-3. 输入用户昵称
-4. 等待下载完成即可. So Easy!
-
-
+## 运行
 ![show](show/screenshot.png)
 
-欢迎Fork, 或者PR  
-要是能骗点Star最好了 :)  
+*运行报错? 很有可能是接口又改了, 来提issue我会及时跟进*
--- weiboPicDownloader.py
+++ weiboPicDownloader.py
@@ -5,14 +5,9 @@ Created on Fri Sep 15 01:38:07 2017
 @author: nondanee
 """
 
-import os
-import sys
-import locale
-import time
-import json
-import re
-import ssl
-import urllib2
+import os, sys, locale
+import json, re, time
+import ssl, urllib2
 import concurrent.futures
 
 try:
@@ -22,7 +17,6 @@ except:
     pass
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
-WEIBO_AMOUNT = 0
 PIC_AMOUNT = 0
 SAVE_PATH = ""
 
@@ -33,7 +27,7 @@ def print_fit(string,flush=False):
     else:
         print string.decode("utf-8").encode(sys.stdin.encoding or locale.getpreferredencoding(True))
         
-def raw_input_fit(string = ""):
+def raw_input_fit(string=""):
     prompt = string.decode("utf-8").encode(sys.stdin.encoding or locale.getpreferredencoding(True))
     return raw_input(prompt).decode(sys.stdin.encoding or locale.getpreferredencoding(True))
     
@@ -44,54 +38,42 @@ def open_and_read(url,max_retry):
         try:            
             response = urllib2.urlopen(request,timeout = 5)
             return response.read()
-        except Exception as e:
+        except:
             retry = retry + 1
-            if retry > max_retry:
+            if retry > max_retry: 
                 return
 
-def get_img_urls(containerid,page):
-    count = 25
-    url = "https://m.weibo.cn/api/container/getIndex?count={}&page={}&containerid={}".format(count,page,containerid)
-    data = open_and_read(url = url,max_retry = 3)
-    if data == None:
-        return []
-
-    jsondata = json.loads(data)
-    if len(jsondata["cards"]) == 0:
-        return
-
+def get_img_urls(containerid):
+    page = 1
+    amount = 0
+    total = 0
     urls = []
-    global WEIBO_AMOUNT
-    
-    for card in jsondata["cards"]:
-        WEIBO_AMOUNT = WEIBO_AMOUNT + 1
-        print_fit("分析微博中: {}".format(WEIBO_AMOUNT),flush=True)
-        if "mblog" in card:
-            if "pics" in card["mblog"]:
-                for pic in card["mblog"]["pics"]:
-                    if "large" in pic:
-                        urls.append(pic["large"]["url"])
+    while True:
+        url = "https://m.weibo.cn/api/container/getIndex?count={}&page={}&containerid={}".format(25,page,containerid)
+        data = open_and_read(url = url,max_retry = 3)
+        if data == None: continue
+        json_data = json.loads(data)
+        if json_data["ok"] != 1: break
+        total = json_data["data"]["cardlistInfo"]["total"]
+        cards = json_data["data"]["cards"]
+        for card in cards:
+            amount = amount + 1
+            print_fit("分析微博中... {}".format(amount),flush=True)
+            if "mblog" in card:
+                if "pics" in card["mblog"]:
+                    for pic in card["mblog"]["pics"]:
+                        if "large" in pic:
+                            urls.append(pic["large"]["url"])
+        page = page + 1
+        time.sleep(1)
+        
+    print_fit("\n分析完毕, 微博总数 {}, 实际获得 {}".format(total,amount))
     return urls
 
 def uid_to_containerid(uid):
     if re.search(r'^\d{10}$',uid) == None:
         return
     else:
-        return "107603" + uid
-       
-def username_to_containerid(name):
-    url = "https://weibo.cn/" + name.encode("utf-8")
-    requset = urllib2.Request(url = url, headers = {'User-Agent' : USER_AGENT})
-    try:
-        response = urllib2.urlopen(requset)
-    except urllib2.HTTPError:
-        return
-    htmlback = response.read()
-    find = re.search(r'<a href="/(\d{10})/info">',htmlback)
-    if find == None:
-        return
-    else:
-        uid = find.group(1)
         return "107603" + uid
         
 def nickname_to_containerid(nickname):
@@ -109,9 +91,9 @@ def nickname_to_containerid(nickname):
         return "107603" + uid
         
 def get_containerid(account_type):
-    input_tips = ["请输入用户ID:","请输入用户名:","请输入用户昵称:"]
-    error_info = ["用户ID无效\n","无法找到该用户\n","无法找到该用户\n"]
-    functions = [uid_to_containerid,username_to_containerid,nickname_to_containerid]
+    input_tips = ["请输入用户ID: ","请输入用户昵称: "]
+    error_info = ["用户ID无效\n","无法找到该用户\n"]
+    functions = [uid_to_containerid,nickname_to_containerid]
     input_string = raw_input_fit(input_tips[account_type])
     input_string = re.sub("\s","",input_string)
     if input_string == "":
@@ -146,12 +128,11 @@ def get_task(urls,miss):
 
 def main():
     while True:
-        print_fit("请输入图片要保存的地址:")
-        home_path = os.path.realpath(raw_input_fit())
+        home_path = os.path.realpath(raw_input_fit("请输入图片存储位置: "))
         print_fit("选择的目录为 {}".format(home_path.encode("utf-8")))
         if os.path.exists(home_path) == True:
             break
-        confirm = raw_input_fit("该目录不存在, 是否创建?(Y/n):")
+        confirm = raw_input_fit("该目录不存在, 是否创建?(Y/n): ")
         confirm = re.sub("\s","",confirm)
         if confirm == "y" or confirm == "Y" or confirm == "":
             try:
@@ -165,14 +146,14 @@ def main():
             print_fit("请手动创建, 或者更改目录\n")
                 
     while True:
-        print_fit("请输入要下载的账号类型:\n[1]用户ID\n[2]用户名\n[3]用户昵称(建议)")
-        choice = raw_input_fit("(1/2/3):")
+        print_fit("请输入要下载的账号类型:\n[1]用户ID [2]用户昵称")
+        choice = raw_input_fit("(1/2): ")
         try:
             choice = int(choice)
         except:
-            choice = 3
-        if choice not in [1,2,3]:
-            choice = 3
+            choice = 2
+        if choice not in [1,2]:
+            choice = 2
         containerid = get_containerid(choice - 1)
         if containerid != None:
             break  
@@ -181,26 +162,14 @@ def main():
     SAVE_PATH = os.path.join(home_path,containerid[6:]) 
     if os.path.exists(SAVE_PATH) == False:
         os.mkdir(SAVE_PATH)
+
+    urls = get_img_urls(containerid)
     
-    global WEIBO_AMOUNT
-    WEIBO_AMOUNT = 0
-    page = 1
-    urls = []
-    while True:
-        more = get_img_urls(containerid,page)
-        if more != None:
-            urls.extend(more)
-            page = page + 1
-            time.sleep(1)
-        else:
-            break
-    
-    print_fit("分析完毕")
     global PIC_AMOUNT
     PIC_AMOUNT = len(urls)
-    print_fit("图片数量: {}".format(PIC_AMOUNT))
+    print_fit("图片数量 {}".format(PIC_AMOUNT))
     
-    max_workers = raw_input_fit("设置最大下载线程数(1-20):")
+    max_workers = raw_input_fit("设置下载线程数(1-20): ")
     try:
         max_workers = int(max_workers)
     except:
@@ -209,7 +178,6 @@ def main():
         max_workers = 20
     elif max_workers < 1:
         max_workers = 1
-    print_fit("最大下载线程数为 {}".format(max_workers))
             
 #    pool = concurrent.futures.ThreadPoolExecutor(max_workers = max_workers)
 #    futures = []
@@ -232,7 +200,7 @@ def main():
             print_fit("已处理 {dealt}/{amount}, 下载失败 {failed}/{amount}".format(dealt=index+1,failed=len(miss),amount=PIC_AMOUNT),flush=True)
                 
         if len(miss) != 0:
-            confirm = raw_input_fit("是否继续尝试?(Y/n):")
+            confirm = raw_input_fit("是否继续尝试?(Y/n): ")
             confirm = re.sub("\s","",confirm)
             if confirm == "y" or confirm == "Y" or confirm == "":
                 continue
